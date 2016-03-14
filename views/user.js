057df044167586ad4282ba3ad586717d44b6217f
hope6537 <hope6537@qq.com>
2016-03-14 20:49:34 +0800

    [reformat]

--- views/user.js
+++ views/user.js
@@ -12,177 +12,10 @@ var {
     StyleSheet,
     TouchableOpacity,
     AsyncStorage,
-    TabBarIOS,
-    NavigatorIOS,
-    AppRegistry,
-    Image,
-    TextInput,
-    StatusBarIOS,
-    TouchableHighlight,
-    ActivityIndicatorIOS,
-    AlertIOS,
     } = React;
 
 
 var User = React.createClass({
-
-    /**
-     * 初始值
-     * @returns {{selectedTab: string, showIndex: {height: number, opacity: number}, showLogin: {flex: number, opacity: number}, isLoadingShow: boolean}}
-     * */
-    getInitialState: function () {
-        return {
-            showIndex: {
-                height: 0,
-                opacity: 0
-            },
-            showLogin: {
-                flex: 1,
-                opacity: 1
-            },
-            isLoadingShow: false
-        };
-    },
-
-    /**
-     * 验证token是否合法
-     */
-    componentDidMount: function () {
-        var that = this;
-        AsyncStorage.getItem('token', function (err, token) {
-            if (!err && token) {
-                var path = Service.host + Service.loginByToken;
-                Util.post(path, {
-                    token: token
-                }, function (data) {
-                    if (data.status) {
-                        that.setState({
-                            showLogin: {
-                                height: 0,
-                                width: 0,
-                                flex: 0,
-                            },
-                            showIndex: {
-                                flex: 1,
-                                opacity: 1
-                            },
-                            isLoadingShow: false
-                        });
-                    }
-                });
-            } else {
-                that.setState({
-                    showIndex: {
-                        height: 0,
-                        opacity: 0
-                    },
-                    showLogin: {
-                        flex: 1,
-                        opacity: 1
-                    },
-                    isLoadingShow: false
-                });
-            }
-        });
-
-        var path = Service.host + Service.getMessage;
-        var that = this;
-        Util.post(path, {
-            key: Util.key
-        }, function (data) {
-            that.setState({
-                data: data
-            });
-        });
-    },
-
-    _getEmail: function (val) {
-        var email = val;
-        this.setState({
-            email: email
-        });
-    },
-
-    _getPassword: function (val) {
-        var password = val;
-        this.setState({
-            password: password
-        });
-    },
-
-    _login: function () {
-        var email = this.state.email;
-        var password = this.state.password;
-        var path = Service.host + Service.login;
-        var that = this;
-
-        //隐藏登录页 & 加载loading
-        that.setState({
-            showLogin: {
-                height: 0,
-                width: 0,
-                flex: 0,
-            },
-            isLoadingShow: true
-        });
-        AdSupportIOS.getAdvertisingTrackingEnabled(function () {
-            AdSupportIOS.getAdvertisingId(function (deviceId) {
-                Util.post(path, {
-                    email: email,
-                    password: password,
-                    deviceId: deviceId,
-                }, function (data) {
-                    if (data.status) {
-                        var user = data.data;
-                        //加入数据到本地
-                        AsyncStorage.multiSet([
-                            ['username', user.username],
-                            ['token', user.token],
-                            ['userid', user.userid],
-                            ['email', user.email],
-                            ['tel', user.tel],
-                            ['partment', user.partment],
-                            ['tag', user.tag],
-                        ], function (err) {
-                            if (!err) {
-                                that.setState({
-                                    showLogin: {
-                                        height: 0,
-                                        width: 0,
-                                        flex: 0,
-                                    },
-                                    showIndex: {
-                                        flex: 1,
-                                        opacity: 1
-                                    },
-                                    isLoadingShow: false
-                                });
-                            }
-                        });
-
-                    } else {
-                        AlertIOS.alert('登录', '用户名或者密码错误');
-                        that.setState({
-                            showLogin: {
-                                flex: 1,
-                                opacity: 1
-                            },
-                            showIndex: {
-                                height: 0,
-                                width: 0,
-                                flex: 0,
-                            },
-                            isLoadingShow: false
-                        });
-                    }
-                });
-            }, function () {
-                AlertIOS.alert('设置', '无法获取设备唯一标识');
-            });
-        }, function () {
-            AlertIOS.alert('设置', '无法获取设备唯一标识，请关闭设置->隐私->广告->限制广告跟踪');
-        });
-    },
 
     render: function () {
         var colors = ['#F4000B', '#17B4FF', '#FFD900', '#F00000'];
@@ -203,57 +36,20 @@ var User = React.createClass({
 
 
         return (
-            <View>
+            <ScrollView style={styles.container}>
+                <View style={styles.wrapper}>
+                    {JSXDOM}
+                </View>
 
-                {this.state.isLoadingShow ?
-                    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-                        <ActivityIndicatorIOS size="small" color="#268DFF"></ActivityIndicatorIOS>
-                    </View> : null
-                }
-                {!this.state.isLoadingShow ?
-                    <View style={this.state.showIndex}>
-                        <ScrollView style={styles.container}>
-                            <View style={styles.wrapper}>
-                                {JSXDOM}
-                            </View>
-
-                            <View style={{marginTop:30}}>
-                                <TouchableOpacity onPress={this._clear}>
-                                    <View style={[styles.item, {flexDirection:'row'}]}>
-                                        <Text style={[styles.tag, {color: colors[i]}]}>Q</Text>
-                                        <Text style={[styles.font,{flex:1}]}>退出登录</Text>
-                                    </View>
-                                </TouchableOpacity>
-                            </View>
-                        </ScrollView>
-                    </View>
-                    : null
-                }
-
-
-                <ScrollView style={[this.state.showLogin]}>
-                    <View style={styles.container}>
-                        <View>
-                            <Image style={styles.logo} source={require('image!logo')}></Image>
+                <View style={{marginTop:30}}>
+                    <TouchableOpacity onPress={this._clear}>
+                        <View style={[styles.item, {flexDirection:'row'}]}>
+                            <Text style={[styles.tag, {color: colors[i]}]}>Q</Text>
+                            <Text style={[styles.font,{flex:1}]}>退出登录</Text>
                         </View>
-
-                        <View style={styles.inputRow}>
-                            <Text>邮箱</Text><TextInput style={styles.input} placeholder="请输入邮箱"
-                                                      onChangeText={this._getEmail}/>
-                        </View>
-                        <View style={styles.inputRow}>
-                            <Text>密码</Text><TextInput style={styles.input} placeholder="请输入密码" password={true}
-                                                      onChangeText={this._getPassword}/>
-                        </View>
-
-                        <View>
-                            <TouchableHighlight underlayColor="#fff" style={styles.btn} onPress={this._login}>
-                                <Text style={{color:'#fff'}}>登录</Text>
-                            </TouchableHighlight>
-                        </View>
-                    </View>
-                </ScrollView>
-            </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         );
     },
 
@@ -292,40 +88,11 @@ var styles = StyleSheet.create({
     wrapper: {
         marginTop: 30,
     },
-    inputRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 10,
-    },
-    input: {
-        marginLeft: 10,
-        width: 220,
-        borderWidth: Util.pixel,
-        height: 35,
-        paddingLeft: 8,
-        borderRadius: 5,
-        borderColor: '#ccc'
-    },
-    btn: {
-        marginTop: 10,
-        width: 80,
-        height: 35,
-        backgroundColor: '#3BC1FF',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 4,
-    },
     tag: {
         marginLeft: 10,
         fontSize: 16,
         fontWeight: 'bold'
-    },
-    logo: {
-        width: 100,
-        height: 100,
-        resizeMode: Image.resizeMode.contain
-    },
+    }
 });
 
 module.exports = User;
