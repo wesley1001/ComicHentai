05dca14ef031466fea1612fe072cec4dd7a91e52
hope6537 <hope6537@qq.com>
2016-04-09 13:19:46 +0800

    [finish userguide]

--- views/user.js
+++ views/user.js
@@ -1,10 +1,6 @@
 var React = require('react-native');
 var Util = require('./util');
-var AddUser = require('./manager/addUser');
-var ModifyPassword = require('./manager/modifyPassword');
-var DeleteUser = require('./manager/deleteUser');
-var PostMessage = require('./manager/postMessage');
-
+var Service = require('./service')
 var Config = require('./user/config');
 var Download = require('./user/download');
 var Favorite = require('./user/favorite');
@@ -17,16 +13,191 @@ var {
     View,
     Text,
     Image,
+    TextInput,
     ScrollView,
     StyleSheet,
     TouchableOpacity,
     AsyncStorage,
+    TouchableHighlight,
+    ActivityIndicatorIOS,
+    AlertIOS,
     } = React;
 
 
 var User = React.createClass({
 
-    render: function () {
+    getInitialState: function () {
+        return {
+            //默认不显示内容
+            showIndex: {
+                height: 0,
+                opacity: 0
+            },
+            //显示登录界面
+            showLogin: {
+                flex: 1,
+                opacity: 1
+            },
+            isLogin: false,
+            isLoadingShow: false
+        };
+    },
+
+    /**
+     * 载入之前验证是否登录
+     */
+    componentWillMount: function () {
+        var that = this;
+        //获取Token
+        AsyncStorage.getItem('token', function (err, token) {
+            if (!err && token) {
+                //如果正常
+                var path = Service.host + Service.loginByToken;
+                //验证Token
+                Util.post(path, {
+                    token: token
+                }, function (data) {
+                    if (data.status) {
+                        //变更显示状态
+                        that.setState({
+                            showLogin: {
+                                height: 0,
+                                width: 0,
+                                flex: 0,
+                            },
+                            showIndex: {
+                                flex: 1,
+                                opacity: 1
+                            },
+                            isLogin: true,
+                            isLoadingShow: false
+                        });
+                    }
+                });
+            }
+            //否则,显示要求登录
+            else {
+                that.setState({
+                    showIndex: {
+                        height: 0,
+                        opacity: 0
+                    },
+                    showLogin: {
+                        flex: 1,
+                        opacity: 1
+                    },
+                    isLogin: false,
+                    isLoadingShow: false
+                });
+            }
+        });
+    },
+
+    /**
+     * 获得用户名
+     * @param val
+     * @private
+     */
+    _getEmail: function (val) {
+        var email = val;
+        this.setState({
+            email: email
+        });
+    },
+
+    /**
+     * 获得密码
+     * @param val
+     * @private
+     */
+    _getPassword: function (val) {
+        var password = val;
+        this.setState({
+            password: password
+        });
+    },
+
+    /**
+     * 登录操作
+     * @private
+     */
+    _login: function () {
+        var email = this.state.email;
+        var password = this.state.password;
+        var path = Service.host + Service.login;
+        var that = this;
+
+        //隐藏登录页 & 加载loading
+        that.setState({
+            showLogin: {
+                height: 0,
+                width: 0,
+                flex: 0,
+            },
+            isLoadingShow: true
+        });
+        AdSupportIOS.getAdvertisingTrackingEnabled(function () {
+            AdSupportIOS.getAdvertisingId(function (deviceId) {
+                Util.post(path, {
+                    email: email,
+                    password: password,
+                    deviceId: deviceId,
+                }, function (data) {
+                    if (data.status) {
+                        var user = data.data;
+                        //加入数据到本地
+                        AsyncStorage.multiSet([
+                            ['username', user.username],
+                            ['token', user.token],
+                            ['userid', user.userid],
+                            ['email', user.email],
+                            ['tel', user.tel],
+                            ['partment', user.partment],
+                            ['tag', user.tag],
+                        ], function (err) {
+                            if (!err) {
+                                that.setState({
+                                    showLogin: {
+                                        height: 0,
+                                        width: 0,
+                                        flex: 0,
+                                    },
+                                    showIndex: {
+                                        flex: 1,
+                                        opacity: 1
+                                    },
+                                    isLogin: true,
+                                    isLoadingShow: false
+                                });
+                            }
+                        });
+
+                    } else {
+                        AlertIOS.alert('登录', '用户名或者密码错误');
+                        that.setState({
+                            showLogin: {
+                                flex: 1,
+                                opacity: 1
+                            },
+                            showIndex: {
+                                height: 0,
+                                width: 0,
+                                flex: 0,
+                            },
+                            isLogin: false,
+                            isLoadingShow: false
+                        });
+                    }
+                });
+            }, function () {
+                AlertIOS.alert('设置', '无法获取设备唯一标识');
+            });
+        }, function () {
+            AlertIOS.alert('设置', '无法获取设备唯一标识，请关闭设置->隐私->广告->限制广告跟踪');
+        });
+    },
+
+    renderUserInfo: function () {
         var colors = ['#F4000B', '#17B4FF', '#FFD900', '#F00000', '#66CCFF'];
         var tags = ['S', 'F', 'H', 'D', 'C'];
         var items = ['我的专题', '我的收藏', '历史记录', '离线下载', '设置'];
@@ -77,6 +248,47 @@ var User = React.createClass({
         );
     },
 
+    renderLogin: function () {
+        return (<ScrollView style={[this.state.showLogin]}>
+            <View style={styles.loginContainer}>
+                <View>
+                    <Image style={styles.logo} source={require('image!logo')}/>
+                </View>
+
+                <View style={styles.inputRow}>
+                    <Text>邮箱</Text><TextInput style={styles.input} placeholder="请输入邮箱"
+                                              onChangeText={this._getEmail}/>
+                </View>
+                <View style={styles.inputRow}>
+                    <Text>密码</Text><TextInput style={styles.input} placeholder="请输入密码" password={true}
+                                              onChangeText={this._getPassword}/>
+                </View>
+
+                <View>
+                    <TouchableHighlight underlayColor="#fff" style={styles.btn} onPress={this._login}>
+                        <Text style={{color:'#fff'}}>登录</Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+        </ScrollView>);
+    },
+
+    render: function () {
+        return (
+            <View style={{flex:1}}>
+                {this.state.isLoadingShow ?
+                    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                        <ActivityIndicatorIOS size="Large" color="#268DFF"/>
+                    </View> : null
+                }
+                {!this.state.isLogin ?
+                    this.renderLogin() : this.renderUserInfo()
+
+                }
+            </View>
+        );
+    },
+
     _loadPage: function (component, title) {
         this.props.navigator.push({
             title: title,
@@ -95,6 +307,40 @@ var User = React.createClass({
 });
 
 var styles = StyleSheet.create({
+    loginContainer:{
+        marginTop: 50,
+        marginBottom: 150,
+        alignItems: 'center',
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        resizeMode: Image.resizeMode.contain
+    },
+    inputRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    input: {
+        marginLeft: 10,
+        width: 220,
+        borderWidth: Util.pixel,
+        height: 35,
+        paddingLeft: 8,
+        borderRadius: 5,
+        borderColor: '#ccc'
+    },
+    btn: {
+        marginTop: 10,
+        width: 80,
+        height: 35,
+        backgroundColor: '#3BC1FF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+    },
     container: {
         flex: 1,
         backgroundColor: '#F5F5F5',
